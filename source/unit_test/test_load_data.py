b95d1284f5e4031af266a6e257118aa9daadebf3
Joseph2505 <joseph.xu@efrei.net>
2024-02-07 20:24:28 +0100

    priere

--- source/unit_test/test_load_data.py
+++ source/unit_test/test_load_data.py
@@ -1,26 +1,20 @@
 import unittest
 import pandas as pd
 from io import StringIO
-import sys
-import os
-
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'streamlit')))
-
-from streamlit.load_data import load_data, convert_columns_to_numeric
-
+import streamlit.load_data as ld
 class TestDataFunctions(unittest.TestCase):
     
     def test_convert_columns_to_numeric(self):
         test_data = StringIO("""A,B,C\n1,2,3\n4,five,6""")
         df = pd.read_csv(test_data, sep=",")
         
-        df_converted = convert_columns_to_numeric(df, ['A', 'B', 'C'])
+        df_converted = ld.convert_columns_to_numeric(df, ['A', 'B', 'C'])
         
         self.assertEqual(df_converted['B'].dtype, 'int64')
         self.assertEqual(df_converted['B'][1], 0) 
         
     def test_load_data(self):
-        df_loaded = load_data('../../dataset/transfusion.data')
+        df_loaded = ldload_data('../../dataset/transfusion.data')
         self.assertIn('target', df_loaded.columns)
 
 if __name__ == '__main__':
