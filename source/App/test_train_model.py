bcd797ee19c11c858525d33025fc42dfadc1d0d8
Joseph2505 <joseph.xu@efrei.net>
2024-02-07 21:12:49 +0100

    corrections bring to unit test

--- source/App/test_train_model.py
+++ source/App/test_train_model.py
@@ -1,10 +1,11 @@
 import unittest
 from sklearn.datasets import make_classification
-from train_model import train_model as tm
+from train_model import train_model
+
 class TestTrainModel(unittest.TestCase):
     def test_train_model(self):
         X, y = make_classification(n_samples=100, n_features=4, n_classes=2, random_state=24)
-        clf, X_test, y_test = tm.train_model(X, y)
+        clf, X_test, y_test = train_model(X, y)
         self.assertIsNotNone(clf)
         
         self.assertEqual(len(X_test), 20)
